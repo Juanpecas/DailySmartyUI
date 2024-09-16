18586e76cb3c7202babb1b5a70c038b654cd3deb
Juanpecas <dan1234loaiza@gmail.com>
2024-09-16 22:37:08 +0200

    setup redux devtools and fixed reducer for set_recent_posts

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import thunk from 'redux-thunk';
 
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(thunk)(compose((window.devToolsExtension ? window.devToolsExtension() : f => f)(createStore)));
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './style/main.scss';
@@ -19,7 +21,7 @@ function main() {
     <Provider store={createStoreWithMiddleware(reducers)}>
       <BrowserRouter>
         <Switch>
-            <Route path='/' component={Home}/>
+            <Route path='/' exact component={Home}/>
             <Route path='/results' component={Results}/>
         </Switch>
       </BrowserRouter>
