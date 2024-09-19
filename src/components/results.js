92776bf49f252047b76ccde6d6a7ee1b1f699a58
Juanpecas <dan1234loaiza@gmail.com>
2024-09-19 21:48:03 +0200

    styled results component

--- src/components/results.js
+++ src/components/results.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import Logo from './logo';
 import SearchBar from './searchBar';
+import ResultsPosts from './resultsPosts';
 
 import { connect } from 'react-redux';
 import * as actions from '../actions';
@@ -14,11 +15,13 @@ class Results extends Component {
 
     render() {
         return (
-            <div>
+            <div className="results">
                 <Logo size={55}/>
                 <SearchBar onSubmit={(query) => this.handleSearchBarSubmit(query)}/>
+                <ResultsPosts/>
             </div>
         )
     }
 }
+
 export default connect(null, actions)(Results);
